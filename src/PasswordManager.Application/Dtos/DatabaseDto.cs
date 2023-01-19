a99b77d3d1b26e78c08fa720b76e32626f308834
Rukin Danil <rukin2018d@gmail.com>
2023-01-19 17:37:59 +0700

    results

---
+++ src/PasswordManager.Application/Dtos/DatabaseDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordManager.Application.Dtos
+{
+    public class DatabaseDto
+    {
+        public string Name { get; set; } = "";
+        public int Id { get; set; }
+        public List<int> RecordIds { get; set; } = new List<int>();
+        public List<int> GroupIds { get; set; } = new List<int>();
+    }
+}
---
+++ src/PasswordManager.Application/Dtos/RecordDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordManager.Application.Dtos
+{
+    public class RecordDto
+    {
+        public int Id { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime LastModifiedDate { get; set; }
+        public DateTime LastAccessDate { get; set; }
+        public string Description { get; set; } = "";
+        public string ResourceName { get; set; } = "";
+        public string ResourcePasswordHash { get; set; } = "";
+        public string ResourceUrl { get; set; } = "";
+
+        public int? RecordContainerId { get; set; } = 0;
+
+        public RecordDto(DateTime creationDate, DateTime lastAccessDate, DateTime lastModifiedDate)
+        {
+            CreationDate = creationDate;
+            LastAccessDate = lastAccessDate;
+            LastModifiedDate = lastModifiedDate;
+        }
+    }
+}
