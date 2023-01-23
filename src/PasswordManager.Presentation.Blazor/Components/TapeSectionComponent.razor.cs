aec5e95285777b26aad5b4d5837204f7f0750927
Rukin Danil <rukin2018d@gmail.com>
2023-01-23 20:17:28 +0700

    toolbar/tape

---
+++ src/PasswordManager.Presentation.Blazor/Components/TapeSectionComponent.razor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace PasswordManager.Presentation.Blazor.Components
+{
+    public partial class TapeSectionComponent
+    {
+        [Parameter]
+        public string SectionName { get; set; } = nameof(SectionName);
+
+        [Parameter]
+        public string FirstButtonIcon { get; set; } = Icons.Material.Filled.Add;
+
+        [Parameter]
+        public string SecondButtonIcon { get; set; } = Icons.Material.Filled.Edit;
+
+        [Parameter]
+        public string ThirdButtonIcon { get; set; } = Icons.Material.Filled.Delete;
+    }
+}
