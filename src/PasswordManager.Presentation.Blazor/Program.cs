4ae7a699d8e185512bc07949721d0ac18c1b6a70
Rukin Danil <rukin2018d@gmail.com>
2023-04-18 22:42:50 +0700

    ui fixes and password generator dialog

--- src/PasswordManager.Presentation.Blazor/Program.cs
+++ src/PasswordManager.Presentation.Blazor/Program.cs
@@ -1,9 +1,17 @@
+using MediatR;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.EntityFrameworkCore;
+using MudBlazor;
 using MudBlazor.Services;
+using PasswordManager.Application;
+using PasswordManager.Application.Dtos;
+using PasswordManager.Application.Services.Interfaces;
 using PasswordManager.Data;
 using PasswordManager.Presentation.Blazor;
+using PasswordManager.Presentation.Blazor.Services.Mappers;
+using PasswordManager.Presentation.Blazor.ViewModels;
+using System.Reflection;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -17,7 +25,22 @@ string migrationAssembly = "PasswordManager.Data";
 //    optionBuilder.UseSqlite(config.GetConnectionString("SqLite"),
 //        sql => sql.MigrationsAssembly(migrationAssembly));
 //});
+
+builder.Services.AddApplicationServices();
+builder.Services.AddMediatR(typeof(Marker).GetTypeInfo().Assembly);
+builder.Services.AddScoped<IMapper<PasswordOptionsViewModel, PasswordOptionsDto>, PasswordOptionsViewModelToPasswordOptionsDtoMapper>();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddMudServices();
+builder.Services.AddMudServices(config =>
+{
+    config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomLeft;
+
+    config.SnackbarConfiguration.PreventDuplicates = false;
+    config.SnackbarConfiguration.NewestOnTop = false;
+    config.SnackbarConfiguration.ShowCloseIcon = true;
+    config.SnackbarConfiguration.VisibleStateDuration = 10000;
+    config.SnackbarConfiguration.HideTransitionDuration = 500;
+    config.SnackbarConfiguration.ShowTransitionDuration = 500;
+    config.SnackbarConfiguration.SnackbarVariant = Variant.Filled;
+});
 
 await builder.Build().RunAsync();
---
+++ src/PasswordManager.Presentation.Blazor/Services/Mappers/PasswordOptionsViewModelToPasswordOptionsDtoMapper.cs
@@ -0,0 +1,34 @@
+﻿using PasswordManager.Application.Dtos;
+using PasswordManager.Application.Services.Interfaces;
+using PasswordManager.Presentation.Blazor.ViewModels;
+
+namespace PasswordManager.Presentation.Blazor.Services.Mappers
+{
+    public class PasswordOptionsViewModelToPasswordOptionsDtoMapper : IMapper<PasswordOptionsViewModel, PasswordOptionsDto>
+    {
+        public PasswordOptionsDto Map(PasswordOptionsViewModel source)
+        {
+            PasswordOptionsDto dto = new()
+            {
+                CountOfCharacters = source.CountOfCharacters,
+                CountOfNumbers = source.CountOfNumbers,
+                Length = source.Length,
+                LowerCaseOnly = source.LowerCaseOnly,
+                UpperCaseOnly = source.UpperCaseOnly,
+                SpecialSymbols = StringToCollectionOfChars(source.SpecialSymbols),
+            };
+            return dto;
+        }
+
+        private List<char> StringToCollectionOfChars(string str, string delimeter = " ")
+        {
+            string[] chars = str.Split(delimeter, StringSplitOptions.RemoveEmptyEntries);
+            List<char> result = new List<char>();
+            for (int i = 0; i < chars.Length; i++)
+            {
+                result.Add(Convert.ToChar(chars[i]));
+            }
+            return result;
+        }
+    }
+}
